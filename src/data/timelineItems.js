e9891dd9a36a7800c645a1af03c9789e3e3695a6
James Dow <photodow@gmail.com>
2021-09-23 13:12:43 -0500

    updated data

--- src/data/timelineItems.js
+++ src/data/timelineItems.js
@@ -112,7 +112,102 @@ export const timelineItems = [
     start: "2021-07-01",
     end: "2021-07-30",
     name: "Gilligan's Island",
+  },{
+    id: 111,
+    start: "2021-07-14",
+    end: "2021-01-22",
+    name: "Recruit translators",
   },
+  {
+    id: 112,
+    start: "2021-08-17",
+    end: "2021-01-31",
+    name: "Create lesson plan 1",
+  },
+  {
+    id: 113,
+    start: "2021-09-05",
+    end: "2021-02-13",
+    name: "Translate phrases for lesson 1",
+  },
+  {
+    id: 114,
+    start: "2021-07-07",
+    end: "2021-03-08",
+    name: "Create dark mode design",
+  },
+  {
+    id: 115,
+    start: "2021-08-14",
+    end: "2021-02-22",
+    name: "Recruit copyeditors",
+  },
+  {
+    id: 116,
+    start: "2021-09-18",
+    end: "2021-02-24",
+    name: "Proofread translations for lesson 1",
+  },
+  {
+    id: 117,
+    start: "2021-07-20",
+    end: "2021-02-22",
+    name: "Finalize logo",
+  },
+  {
+    id: 118,
+    start: "2021-08-21",
+    end: "2021-03-22",
+    name: "Implement dark mode",
+  },
+  {
+    id: 119,
+    start: "2021-09-21",
+    end: "2021-02-28",
+    name: "Finalize lesson plan 1",
+  },
+  {
+    id: 1110,
+    start: "2021-10-23",
+    end: "2021-02-23",
+    name: "Approve logo",
+  },
+  {
+    id: 1111,
+    start: "2021-11-03",
+    end: "2021-03-29",
+    name: "Create lesson plan 2",
+  },
+  {
+    id: 1112,
+    start: "2021-12-30",
+    end: "2021-04-08",
+    name: "Translate phrases for lesson 2",
+  },
+  {
+    id: 1113,
+    start: "2021-10-01",
+    end: "2021-04-04",
+    name: "Debug mobile notification error",
+  },
+  {
+    id: 1114,
+    start: "2021-11-05",
+    end: "2021-04-06",
+    name: "Test debugged mobile notifications",
+  },
+  {
+    id: 1115,
+    start: "2021-12-16",
+    end: "2021-04-30",
+    name: "Beta test",
+  },
+  {
+    id: 1116,
+    start: "2021-10-01",
+    end: "2021-05-01",
+    name: "Launch day",
+  }
 ];
 
 export default timelineItems;
